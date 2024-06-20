bcacd9a93acde173ed2a2db8d6299f50f5e245c1
rrrtent <134557648+rrrtent@users.noreply.github.com>
2024-06-20 16:46:32 -0300

    Add files via upload

---
+++ ChallengeLiteraAlura ONE/backup/src/main/java/com/example/ChallengeLiteraAlura/ONE/ChallengeLiteraAluraOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChallengeLiteraAlura.ONE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLiteraAluraOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLiteraAluraOneApplication.class, args);
+	}
+
+}
